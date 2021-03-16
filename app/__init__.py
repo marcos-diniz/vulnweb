d0d4fff250154003ebc8e9e888a050c716bf07c4
Marcos Diniz <marcos1930_@outlook.com>
2021-03-15 23:29:38 -0300

    adding routes

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,23 @@
-from flask import Flask
-
+from flask import Flask, request, redirect, current_app, render_template, session
 
 def create_app():
   app = Flask(__name__)
-  
+
   @app.route('/')
   @app.route('/index')
   def index():
-    return "<h1>Hello World!</h1>"
-  
-  
-  
+    return "index page"
+
+  @app.route('/login', methods=["GET", "POST"])
+  def login():
+    return "profile page"
+
+  @app.route('/profile', methods=["GET"])
+  def profile():
+    return "profile page"
+
+  @app.route('/signup', methods=["POST", "GET"])
+  def signup():
+    return "signup page"
+
   return app
